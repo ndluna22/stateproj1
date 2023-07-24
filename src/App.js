e8573c93a784edc7dfc200456f13cb7d62794479
Nadia Luna <ndaluna@gmail.com>
2023-07-23 22:39:30 -0500

    react state exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./EighBall.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <EightBall />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/EightBall.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+//choose random answer everytime
+function randomanswers(items) {
+  let idx = Math.floor(Math.random() * items.length);
+  return items[idx];
+}
+
+//initial:  { msg: "Think of a Question", color: "black" },
+function EightBall(props) {
+  const [msg, setMsg] = useState("Think of a Question.");
+  const [color, setColor] = useState("black");
+
+  function handleClick() {
+    const { msg, color } = randomanswers(props.answers);
+    setMsg(msg);
+    setColor(color);
+  }
+
+  return (
+    <div
+      className="EightBall"
+      onClick={handleClick}
+      style={{ backgroundColor: color }}
+    >
+      <b>{msg}</b>
+    </div>
+  );
+}
+
+EightBall.defaultProps = {
+  defaultanswers: [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" },
+  ],
+};
+
+export default EightBall;
